c932d8e05846f75d507b30a1bd1364a014c396dd
XiaoAnQing <50309602+XiaoAnQing@users.noreply.github.com>
2019-12-19 19:19:04 +0800

    WEWE

--- src/main/java/com/jhy/plateform/PlateformApplication.java
+++ src/main/java/com/jhy/plateform/PlateformApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PlateformApplication {
 
     public static void main(String[] args) {
+        //
         SpringApplication.run(PlateformApplication.class, args);
     }
 
